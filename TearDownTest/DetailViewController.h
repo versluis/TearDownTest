8b59ef837f967d55f27720bd1241191f351e2893
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-04-09 08:14:48 -0400

    Initial Commit

---
+++ TearDownTest/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  TearDownTest
+//
+//  Created by Jay Versluis on 09/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
